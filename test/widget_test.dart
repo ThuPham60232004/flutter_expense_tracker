4af8556cff1ea17f9ef278e24a9fde410d940ad4
PHẠM THỊ ANH THƯ <165041915+ThuPham60232004@users.noreply.github.com>
2025-01-06 16:29:22 +0700

    fix: thêm test home

---
+++ test/widget_test.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_expense_tracker/presentation/screens/home_screen.dart';
+import 'package:flutter_expense_tracker/core/widgets/widget_appbar.dart';
+import 'package:flutter_expense_tracker/core/widgets/widget_drawer.dart';
+import 'package:flutter_expense_tracker/core/widgets/widget_search.dart';
+import 'package:flutter_expense_tracker/core/widgets/widgte_banner.dart';
+void main() {
+  testWidgets('HomeScreen should display all widgets correctly', (WidgetTester tester) async {
+    // Build the widget tree
+    await tester.pumpWidget(MaterialApp(home: HomeScreen()));
+
+    // Kiểm tra AppBar
+    expect(find.byType(CustomAppBar), findsOneWidget);
+
+    // Kiểm tra Drawer
+    expect(find.byType(CustomDrawer), findsOneWidget);
+
+    // Kiểm tra WidgetSearch
+    expect(find.byType(WidgetSearch), findsOneWidget);
+
+    // Kiểm tra BannerCarousel
+    expect(find.byType(BannerCarousel), findsOneWidget);
+
+    // Kiểm tra Statistics Header
+    expect(find.text('Thống kê hàng tháng'), findsOneWidget);
+    expect(find.byType(Image), findsWidgets);
+
+    // Kiểm tra FadeItems
+    expect(find.byType(FadeItem), findsNWidgets(3));
+
+    // Kiểm tra Monthly Budget Section
+    expect(find.byType(ElevatedButton), findsNWidgets(2));
+  });
+}
