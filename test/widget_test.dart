5984b72f30f1bfe157669fb1d77f3adedbf677bb
PHẠM THỊ ANH THƯ <165041915+ThuPham60232004@users.noreply.github.com>
2025-01-06 18:35:26 +0700

    test: sửa lỗi test 8

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,1 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_expense_tracker/presentation/screens/signup_screen.dart';
 
+void main() {
+  testWidgets('Kiểm tra hiển thị màn hình đăng ký',
+      (WidgetTester tester) async {
+    // Build widget cần test
+    await tester.pumpWidget(const SignUpScreen());
+
+    // Tìm kiếm các widget theo text
+    expect(find.text('Sign Up'), findsOneWidget);
+    expect(find.text('Google'), findsOneWidget);
+    expect(find.text('Apple ID'), findsOneWidget);
+  });
+}
