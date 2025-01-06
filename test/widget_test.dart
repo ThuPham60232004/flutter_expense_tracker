835077fdfe3f31df5baf646834ba67a55b2de2da
PHẠM THỊ ANH THƯ <165041915+ThuPham60232004@users.noreply.github.com>
2025-01-06 18:54:24 +0700

    test: sửa lỗi test 13

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,1 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_expense_tracker/presentation/screens/signup_screen.dart';
 
-void main() {
-  testWidgets('Kiểm tra hiển thị màn hình đăng ký',
-      (WidgetTester tester) async {
-    // Build widget cần test
-    await tester.pumpWidget(SignUpScreen());
-  });
-}
